4f950c5a4edb092dbd9e66a14d3d177460bc8c30
Roman Gromov <hanaevroman@gmail.com>
2019-06-03 10:57:42 +0000

    Resolve "Issue time tracking"

--- src/app/components/issues/issues.component.ts
+++ src/app/components/issues/issues.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RouterExtensions } from 'nativescript-angular';
-import { ios } from 'tns-core-modules/application';
+import { finalize } from 'rxjs/operators';
 import { ItemEventData } from 'tns-core-modules/ui/list-view';
+import { Page } from 'tns-core-modules/ui/page';
 import { IssuesManager } from '~/app/managers/issues.manager';
 import { MeManager } from '~/app/managers/me.manager';
-import { IssueCard, IssuesFilter } from '~/app/models/issue';
+import { IssueCard, IssuesFilter, IssueState } from '~/app/models/issue';
 import { Me } from '~/app/models/me';
 import { IssuesService } from '~/app/services/issues.service';
 
@@ -16,26 +17,44 @@ import { IssuesService } from '~/app/services/issues.service';
 })
 export class IssuesComponent implements OnInit {
 
-    user: Me;
-    issues: IssueCard[];
-    transitions: string[] = [];
+    private _user: Me;
+    issues: IssueCard[] = [];
+    loading = false;
+
+    set user(user: Me) {
+        this._user = user;
+        if (!!user) {
+            this.load();
+
+        } else {
+            this.issues = [];
+        }
+    }
+
+    get user() {
+        return this._user;
+    }
 
     constructor(private issuesService: IssuesService,
                 private router: RouterExtensions,
                 private route: ActivatedRoute,
+                private page: Page,
                 public issuesManager: IssuesManager,
                 public me: MeManager) {
     }
 
     ngOnInit(): void {
-        if (ios) {
-            this.transitions = ['curl', 'curlDown', 'fade', 'flip', 'flipLeft', 'slide', 'slideRight', 'slideTop', 'slideBottom'];
-        } else {
-            this.transitions = ['explode', 'fade', 'flip', 'flipLeft', 'slide', 'slideRight', 'slideTop', 'slideBottom'];
-        }
+        this.page.actionBarHidden = true;
+        this.loading = true;
+        this.me.user$.pipe(finalize(() => this.loading = false))
+            .subscribe(user => this.user = user);
+    }
 
-        this.me.user$.subscribe(user => this.user = user);
-        this.issuesService.list(new IssuesFilter())
+    load() {
+        const filter = new IssuesFilter({user: this.user.id, state: IssueState.opened});
+        this.loading = true;
+        this.issuesService.list(filter)
+            .pipe(finalize(() => this.loading = false))
             .subscribe(paging => this.issues = paging.results);
     }
 
@@ -43,11 +62,7 @@ export class IssuesComponent implements OnInit {
         this.router.navigate([this.issues[item.index].id], {
             relativeTo: this.route,
             animated: true,
-            transition: {
-                name: this.transitions[Math.floor(Math.random() * this.transitions.length)],
-                duration: 380,
-                curve: 'easeIn'
-            }
+            transition: {name: 'slide', duration: 200, curve: 'easeIn'}
         });
     }
 }
--- src/app/components/profile/profile-routing.module.ts
+++ src/app/components/profile/profile-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
-import { IssueDetailComponent } from '~/app/components/issues/issue-detail/issue-detail.component';
-import { IssuesComponent } from '~/app/components/issues/issues.component';
+import { ProfileComponent } from '~/app/components/profile/profile.component';
 
 const routes: Routes = [
     {
         path: '',
-        component: IssuesComponent
-    },
-    {
-        path: ':id',
-        component: IssueDetailComponent
+        component: ProfileComponent
     }
 ];
 
@@ -19,5 +14,5 @@ const routes: Routes = [
     imports: [NativeScriptRouterModule.forChild(routes)],
     exports: [NativeScriptRouterModule]
 })
-export class IssuesRoutingModule {
+export class ProfileRoutingModule {
 }
